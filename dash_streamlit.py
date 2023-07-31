94554e005ef26ce2153938711ebb60d19f92539e
mcerqueira1524 <marcos.cerqueira@ismart.org.br>
2023-07-31 18:00:23 -0300

    Update dash_streamlit.py

--- dash_streamlit.py
+++ dash_streamlit.py
@@ -1294,7 +1294,8 @@ if check_password():
             fig_olimp_olimpiadas.add_bar(x=x, y=df_olimpiadas_filtro_legenda['Quantidade'], name=status,
                                           text=df_olimpiadas_filtro_legenda['Quantidade'], 
                                           textposition='auto',    
-                                          insidetextanchor='middle'  
+                                          insidetextanchor='start' ,
+                                          orientation='v' 
                                           )
             
         df_inscritos = df_olimpiadas[df_olimpiadas['Status da Inscrição'] == 'Inscrito']
