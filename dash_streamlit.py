c4d16eda368613f46d7fb54b5bb159301da510b4
mcerqueira1524 <marcos.cerqueira@ismart.org.br>
2023-08-02 09:31:22 -0300

    Update dash_streamlit.py

--- dash_streamlit.py
+++ dash_streamlit.py
@@ -1297,13 +1297,13 @@ if check_password():
                                           insidetextanchor='start' ,
                                           orientation='v' 
                                           )
+        fig_olimp_olimpiadas.update_layout(barmode="relative")
 
               
         #df_inscritos =  df_olimpiadas[df_olimpiadas['Status da Inscrição'] == 'Inscrito']
         #x_inscritos = [df_inscritos['Olimpíada'].tolist(), df_inscritos['Ano'].astype(str).tolist()]
         #fig_olimp_olimpiadas.add_scatter(x=x_inscritos, y=df_inscritos['Quantidade'], mode='lines+markers', name='Inscritos')
 
-        fig_olimp_olimpiadas.update_layout(barmode="relative")
         
         fig_olimp_olimpiadas.update_layout(
             plot_bgcolor=bg_color_dash,
